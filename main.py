152cf971faae835383aa48e68a2443f5e7167138
Abdulrahmantommy <46884018+Abdulrahmantommy@users.noreply.github.com>
2020-12-12 04:51:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from bs4 import BeautifulSoup
-
 import string
-import webbrowser
 from random import *
-import requests
 import requests.exceptions
 from urllib.parse import urlsplit
 from collections import deque
 import re
 from termcolor import *
-tm=colored("""
-    
-    
+
+tm = colored('\033[1m'+"""
 ##################################################
                                                  #
  ▄▀▀▀▀▄        ▄▀▀▄▀▀▀▄  ▄▀▀█▄   ▄▀▀▀▀▄  ▄▀▀▀▀▄  #
@@ -20,71 +16,71 @@ tm=colored("""
 █     █ █        █       ▄▀   █ ▀▄   █  ▀▄   █   #
 ▐▀▄▄▄▄▀ ▐      ▄▀       █   ▄▀   █▀▀▀    █▀▀▀    #
 ▐             █         ▐   ▐    ▐       ▐       #
-MADY BY TOMMY ▐   "Abdulrahman Tohamy"  V.0.1    #                        
+MADY BY TOMMY ▐   "Abdulrahman Tohamy"  V.0.1    #
 ##################################################
-    
-    """, "red")
+""", "yellow")
 print(tm)
-mixchars = string.ascii_letters+string.digits+string.punctuation
+gt = colored(">>", 'blue')
+v1 = colored('\033[1m'+"[", 'red' )
+#v2 = colored('\033[1m'+">", 'cyan', 'on_red')
+v3 = colored('\033[1m'+"]", 'red')
+#all= print(v1, v2, v3)
+mixchars = string.ascii_letters + string.digits + string.punctuation
 
-
-randompass = input("Gave me Name or Number: ").join(choice(mixchars) for i in range(randint(8, 20)))
-print("Your Random Password is: ")
-ps=colored(randompass, "blue")
+randompass = input(colored('\033[1m'+"Enter your Code : ", 'yellow', 'on_blue')).join(choice(mixchars) for i in range(randint(8, 20)))
+print('\033[1m'+"Yor Code Login To Tool Is : ")
+ps = colored('\033[1m'+randompass, "blue", "on_yellow")
 print(ps)
-# a queue of urls to be crawled
-new_urls = deque(['http://ertu.org/1/friendsReg_frame.asp'])
+lod=colored('\033[1m'+"Loading Tools.... :) ", 'green')
+print(lod)
+new_urls = deque(['http://google.com/', 'http://ertu.org/1/friendsReg_frame.asp'])
 
-#['http://ertu.org/1/friendsReg_frame.asp']
-# a set of urls that we have already crawled
+# ['http://ertu.org/1/friendsReg_frame.asp']
 processed_urls = set()
 
-# a set of crawled emails
 emails = set()
 
-# process urls one by one until we exhaust the queue
 while len(new_urls):
-	# move next url from the queue to the set of processed urls
-	url = new_urls.popleft()
-	processed_urls.add(url)
+    url = new_urls.popleft()
+    processed_urls.add(url)
 
-	# extract base url to resolve relative links
-	parts = urlsplit(url)
-	base_url = "{0.scheme}://{0.netloc}".format(parts)
-	path = url[:url.rfind('/')+1] if '/' in parts.path else url
+    parts = urlsplit(url)
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/') + 1] if '/' in parts.path else url
 
-	# get url's content
 
-	try:
-		tommy = requests.get(url)
-	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-		# ignore pages with errors
-		continue
-
-	# extract all email addresses and add them into the resulting set
-	new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", tommy.text, re.I,))
-	emails.update(new_emails)
-	pr=colored("By Tommy >>  %s"  % new_emails, 'green')
-
-	print(pr)
-
-	# create a beutiful soup for the html document
-	soup = BeautifulSoup(tommy.text, 'html.parser')
-
-	# find and process all the anchors in the document
-	for anchor in soup.find_all("a"):
-		# extract link url from the anchor
-		link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-	# resolve relative links
-	if link.startswith('/'):
-		link = base_url + link
-	elif not link.startswith('http'):
-		link = path + link
-	# add the new url to the queue if it was not enqueued nor processed yet
-	if not link in new_urls and not link in processed_urls:
-		new_urls.append(link)
+    try:
+        tommy = requests.get(url)
+    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+        continue
 
 
 
+    new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", tommy.text, re.I, ))
+    emails.update(new_emails)
+
+    # print(pr)
+
+    for new_emails in new_emails:
+
+        by = colored('\033[1m'+"OK", 'green',)
+        pr = colored(" %s"% '\033[1m'+new_emails,  'yellow')
+
+        error = print('')
 
 
+
+        print(v1, by,v3, pr,  end='\n')
+
+#------
+
+    soup = BeautifulSoup(tommy.text, 'html.parser')
+
+    for anchor in soup.find_all("a"):
+        link = anchor.attrs["href"] if "href" in anchor.attrs else ''
+    if link.startswith('/'):
+        link = base_url + link
+    elif not link.startswith('http'):
+        link = path + link
+    if not link in new_urls and not link in processed_urls:
+        new_urls.append(link)
